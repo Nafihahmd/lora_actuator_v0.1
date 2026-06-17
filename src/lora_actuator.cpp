acf3a8b0bea8c020577e846cdfcbe7caece470af
nafihahmd <nafihahamd@gmail.com>
2026-06-17 20:40:27 +0530

    chore: update LoRa radio configuration parameters in setup

--- src/lora_actuator.cpp
+++ src/lora_actuator.cpp
@@ -66,7 +66,7 @@ void setup()
     ConfigLoRa_t config;
     config.frequency = 865.0;
 
-    int state = radio.begin(865.0, 125.0, 9, 7, RADIOLIB_SX126X_SYNC_WORD_PRIVATE, 10, 8, 0, false);
+    int state = radio.begin(865.0, 125.0, 12, 8, RADIOLIB_SX126X_SYNC_WORD_PRIVATE, 22, 8, 0, false);
 
     if(state != RADIOLIB_ERR_NONE)
     {
